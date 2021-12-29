06621e1c06dc34430e5bec2b73f41a0b4cbccf35
Abel1027 <arodriguezm1027@gmail.com>
2021-12-29 09:29:35 +0100

    Add flutter localizations dependency

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,7 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_localizations/flutter_localizations.dart';
 
+import 'src/presentation/l10n/generated/l10n.dart';
 import 'src/presentation/pages/home_page.dart';
 
 void main() {

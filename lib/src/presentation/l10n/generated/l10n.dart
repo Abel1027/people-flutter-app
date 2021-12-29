94e497a282c58028df438def95f67e7e7ece2e99
Abel1027 <arodriguezm1027@gmail.com>
2021-12-29 09:33:07 +0100

    Create english .arb file

--- lib/src/presentation/l10n/generated/l10n.dart
+++ lib/src/presentation/l10n/generated/l10n.dart
@@ -67,6 +67,7 @@ class AppLocalizationDelegate extends LocalizationsDelegate<S> {
   List<Locale> get supportedLocales {
     return const <Locale>[
       Locale.fromSubtags(languageCode: 'es'),
+      Locale.fromSubtags(languageCode: 'en'),
     ];
   }
 
